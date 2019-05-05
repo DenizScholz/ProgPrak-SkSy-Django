79b9081f62e3003c4fc503d66ab5310ba9711f53
Deniz Scholz <denizscholz@t-online.de>
2019-05-05 19:33:52 +0200

    Created Todo Model and integrated to template

--- django_project/todo/models.py
+++ django_project/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    description = models.CharField(max_length=180)
+    due_date = models.DateTimeField(default=datetime.now, blank=True)
+    done_percentage = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.description
--- django_project/todo/views.py
+++ django_project/todo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Todo
 
 # Create your views here.
 def home(request):
     context = {
         'title': 'Home',
+        'todos': Todo.objects.all(),
     }
     return render(request, 'todo/home.html', context)
 
